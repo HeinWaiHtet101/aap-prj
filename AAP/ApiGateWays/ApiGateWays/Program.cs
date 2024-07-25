440b1933e63615424d1ad5d21ef3c6a15b3a13f0
HEIN WAI HTET <hein.wai.htet@startech.co.jp>
2024-07-25 14:25:44 +0900

    add Yarp for api gateways setup reverse proxy services

--- AAP/ApiGateWays/ApiGateWays/Program.cs
+++ AAP/ApiGateWays/ApiGateWays/Program.cs
@@ -1,6 +1,24 @@
+using Microsoft.AspNetCore.RateLimiting;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Add Services to the Container
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+builder.Services.AddRateLimiter(rateLimiterOptions =>
+{
+    rateLimiterOptions.AddFixedWindowLimiter("fixed", options =>
+    {
+        options.Window = TimeSpan.FromSeconds(10);
+        options.PermitLimit = 5;
+    });
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Configure the HTTP request pipeline
+app.UseRateLimiter();
+app.MapReverseProxy();
 
 app.Run();
