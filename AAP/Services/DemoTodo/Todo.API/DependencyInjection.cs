c4075f55dc12c55ffd1e3dfd1adab0e7d46398c7
HEIN WAI HTET <hein.wai.htet@startech.co.jp>
2024-07-25 15:16:49 +0900

    add Dependency Injection class for each services injection

---
+++ AAP/Services/DemoTodo/Todo.API/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿namespace Todo.API;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApiServices(this IServiceCollection services)
+    {
+        services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+        return services;
+    }
+
+    public static WebApplication UseApiServices(this WebApplication app)
+    {
+        return app;
+    }
+}
